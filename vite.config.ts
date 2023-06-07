cd6e12fcb8e10f3d79c360670b2d1afaaf09ab8f
Ivan <efcolipt@gmail.com>
2023-06-07 20:28:51 +0300

    chore: optimaze packages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig(() => {
   return {
+    optimizeDeps: {
+      include: ["slick-carousel", "jquery"],
+    },
     build: {
       emptyOutDir: true,
       minify: true,

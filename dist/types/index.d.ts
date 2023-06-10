f1708b44ced1e5281a6d4012f97b07362f2339b6
Ivan Baykov <efcolipt@gmail.com>
2023-06-10 15:35:58 +0300

    chore: remove dist

--- dist/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { App } from "vue";
-import VueSlickCarousel from "./VueSlickCarousel.vue";
-export declare function install(app: App): void;
-export { VueSlickCarousel };
-declare const _default: {
-    install: typeof install;
-};
-export default _default;

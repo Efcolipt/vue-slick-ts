e97801be9cb6c5853523960fa935236e38a3985b
Ivan Baykov <efcolipt@gmail.com>
2023-06-10 17:08:55 +0300

    fix: export types carousel

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,17 @@ export function install(app: App) {
   app.component("VueSlickCarousel", VueSlickCarousel);
 }
 
+export type {
+  ListSlickEvents,
+  ListSlickMethods,
+  SlickBreakpoint,
+  SlickDirection,
+  SlickEvents,
+  SlickInstance,
+  SlickInstanceOptions,
+  SlickMethods,
+} from './carousel.types'
+
 export { VueSlickCarousel };
 
 export default {
